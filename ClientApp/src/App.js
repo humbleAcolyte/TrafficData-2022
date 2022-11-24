ddc38a66974c99c6b0d34f02ef472aa061e892fa
humbleacolyte <bayandin.eugene@gmail.com>
2022-11-25 02:56:15 +0500

    Changed main page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import AvrgSpeedChart from './components/AvrgSpeedChart/AvrgSpeedChart';
+import './styles/theme.scss';
+import './styles/widgets.scss';
 
 export default class App extends Component {
   static displayName = App.name;
 
   render() {
     return (
-      <div>
-        <h1>Hello world!</h1>
+      <div className='page'>
+        <AvrgSpeedChart />
       </div>
     );
   }
